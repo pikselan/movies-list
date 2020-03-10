c0f48e3d97a40159e8013846934146b046d2ff9a
pikselan <faif@pikselan.com>
2020-03-10 11:04:57 +0700

    add heroimage popular

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from '../elements/Header/Header';
 import Home from '../Home/Home';
+import NotFound from '../elements/NotFound/NotFound';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Header />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route component={NotFound} />
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
   )
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../../config';
 import './Home.css';
 
 import HeroImage from '../elements/HeroImage/HeroImage';
@@ -10,13 +11,51 @@ import Spinner from '../elements/Spinner/Spinner';
 
 class Home extends Component {
   state = {
+    heroImage: null,
+    movies: [],
+    loading: false,
+    currentPage: 0,
+    totalPages: 0,
+    searchTerm: ''
+  }
 
+  componentDidMount() {
+    this.setState({ loading: true })
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+    this.fetchItems(endpoint)
+  }
+
+  fetchItems = (endpoint) => {
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(result => {
+        this.setState({
+          movies: [...this.state.movies, ...result.results],
+          heroImage: this.state.heroImage || result.results[0],
+          loading: false,
+          currentPage: result.page,
+          totalPages: result.total_pages
+        }, () => {
+
+        })
+      })
+      .catch(err => console.error('error:', err))
   }
 
   render() {
     return (
       <div className="rmdb-home">
-        <HeroImage />
+        {this.state.heroImage ? 
+        
+          <HeroImage
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${this.state.heroImage.backdrop_path}`}
+            title={this.state.heroImage.original_title}
+            text={this.state.heroImage.overview}
+          />
+
+          : null  
+        }
+
         <SearchBar />
         <FourColGrid />
         <Spinner />
